5bba10110f0dc1f571e5d57990b0e22a67c9aa19
LVutner <kkamil724@gmail.com>
2024-08-19 01:56:36 +0200

    * UPD: [Shaders] Updated depth_downsample shader

--- gamedata/shaders/d3d11/depth_downsample.ps.hlsl
+++ gamedata/shaders/d3d11/depth_downsample.ps.hlsl
@@ -6,19 +6,7 @@ struct PSInput
     float4 texcoord : TEXCOORD0;
 };
 
-float4 main(PSInput I) : SV_Target
+float main(PSInput I) : SV_Target
 {
-	float4 Depth;
-
-#ifndef SM_5
-    Depth.x = s_position.SampleLevel(smp_nofilter, I.texcoord.xy, int2(1, 0), 0).x;
-    Depth.y = s_position.SampleLevel(smp_nofilter, I.texcoord.xy, int2(1, 1), 0).x;
-    Depth.z = s_position.SampleLevel(smp_nofilter, I.texcoord.xy, int2(0, 1), 0).x;
-    Depth.w = s_position.SampleLevel(smp_nofilter, I.texcoord.xy, int2(0, 0), 0).x;
-#else // !SM_5
-    Depth = s_position.GatherRed(smp_nofilter, I.texcoord.xy);
-#endif // SM_5
-
-    Depth = depth_unpack.x * rcp(Depth - depth_unpack.y);
-	return min(min(Depth.x, Depth.y), min(Depth.z, Depth.w));
+	return depth_unpack.x * rcp(s_position.SampleLevel(smp_nofilter, I.texcoord.xy, 0).x - depth_unpack.y);
 }
