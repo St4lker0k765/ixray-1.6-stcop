3845ca4e02cac40bdadcc69a83f263e1dc59349a
Drombeys <dima.zolotov02@gmail.com>
2025-05-14 12:31:53 +0300

    Залокали m_ChildWndList
    Co-authored-by: v2v3v4 <terran.erre@mail.ru>

--- src/xrGame/ui/UIRankingsCoC.cpp
+++ src/xrGame/ui/UIRankingsCoC.cpp
@@ -110,6 +110,8 @@ void CUIRankingsCoC::Update()
 }
 bool CUIRankingsCoC::ParentHasMe()
 {
+	xrCriticalSectionGuard guard(m_parent->csUi);
+
 	WINDOW_LIST::const_iterator it = std::find(m_parent->Items().begin(), m_parent->Items().end(), this);
 	return it != m_parent->Items().end();
 }
